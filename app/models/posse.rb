46f01be891ad5ae8f60d4c778305276e36230f04
Alan Smith <a.bax.smith@gmail.com>
2014-11-18 14:14:47 -0700

    adds name validation for posses model and test --all passing

--- app/models/posse.rb
+++ app/models/posse.rb
@@ -1,4 +1,4 @@
 class Posse < ActiveRecord::Base
-
+  validates :name, presence: true
 end
 
---
+++ spec/models/posse_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "the posse model" do
+
+  it "is valid with a name" do
+    posse = Posse.new
+    posse.name = "McCarthy"
+    posse.save
+
+    expect(Posse.all.count).to eq(1)
+    expect(Posse.first.name).to eq("McCarthy")
+  end
+
+  it "is invalid without a name" do
+    posse = Posse.create
+
+    expect(Posse.all.count).to eq(0)
+  end
+
+end
