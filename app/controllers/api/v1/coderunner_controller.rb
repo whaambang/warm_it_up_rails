fcd3149e815364e6bd1cf626f033321f26ea8271
Hilary Denton <Hilarydenton90@gmail.com>
2014-11-20 08:55:34 -0700

    getting coderunner to work

--- app/controllers/api/v1/coderunner_controller.rb
+++ app/controllers/api/v1/coderunner_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::CoderunnerController < ApplicationController
-
-  def create
+  def index
     result = EvalIn.call(params[:code], language: "ruby/mri-2.1")
     answer = result.output.chomp
-    render json: answer
+    render json: answer.to_json, callback: params["callback"]
   end
 end
