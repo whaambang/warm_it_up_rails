62c2d99c61c86558c3718bdb40e36bbb60ff124c
Will Faurot <wfaurot@gmail.com>
2014-11-20 11:30:47 -0700

    Clean up request specs

--- spec/requests/api_requests_spec.rb
+++ spec/requests/api_requests_spec.rb
@@ -1,13 +1,12 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe 'API' do
+describe "API" do
 
   #posse tests
-  it 'returns all posses' do
-    10.times {Posse.create(name: "Posse Name")}
+  it "returns all posses" do
+    10.times { Posse.create(name: "Posse Name") }
     get "/api/v1/posses.json"
     json = JSON.parse(response.body)
-
     expect(json['posses'].count).to eq(10)
   end
 
@@ -17,14 +16,13 @@ describe 'API' do
     posse.save
 
     get "/api/v1/posses/#{posse.id}.json"
-
     json = JSON.parse(response.body)
     expect(json["posse"]["name"]).to eq("McCarthy")
   end
 
   #problem tests
-  it 'returns all problems' do
-    10.times {Problem.create( content: "If a tree falls in the woods and no one is there to hear it, does it make a sound?", answer: "Nope." ) }
+  it "returns all problems" do
+    10.times { Problem.create(content: "If a tree falls in the woods and no one is there to hear it, does it make a sound?", answer: "Nope.") }
     get "/api/v1/problems.json"
     json = JSON.parse(response.body)
 
@@ -45,24 +43,23 @@ describe 'API' do
 
   #solution tests
   it 'returns all solutions' do
-    10.times {Solution.create(problem_id: 1,
-                              posse_id: 1,
-                              content: "things.sort",
-                              points_earned: 0
-                              ) }
+    10.times do
+      Solution.create(problem_id: 1,
+                      posse_id: 1,
+                      content: "things.sort",
+                      points_earned: 0)
+    end
 
     get "/api/v1/solutions.json"
     json = JSON.parse(response.body)
-
     expect(json['solutions'].count).to eq(10)
   end
 
-  it 'returns a specific solution' do
+  it "returns a specific solution" do
     solution = Solution.create(problem_id: 1,
-                              posse_id: 1,
-                              content: "things.sort",
-                              points_earned: 0
-                              )
+                               posse_id: 1,
+                               content: "things.sort",
+                               points_earned: 0)
 
     get "/api/v1/solutions/#{solution.id}.json"
 
@@ -71,9 +68,9 @@ describe 'API' do
   end
 
   #evalulate code test
-  it 'returns evaluated code' do
-    get '/api/v1/coderunner', { code: "puts 'hello world'" }
+  it "returns evaluated code" do
+    get "/api/v1/coderunner", { code: "puts 'hello world'" }
     resp = response.body.delete('"')
-    expect(resp).to eq('hello world')
+    expect(resp).to eq("hello world")
   end
 end
