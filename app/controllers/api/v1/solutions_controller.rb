8bbf71371664bdb585d8a6158a8622f22606239e
Allison Larson <allisonlarson91@gmail.com>
2014-12-02 10:23:15 -0700

    only first solution for the day saves

--- app/controllers/api/v1/solutions_controller.rb
+++ app/controllers/api/v1/solutions_controller.rb
@@ -11,7 +11,10 @@ class Api::V1::SolutionsController < ApplicationController
   end
 
   def create
-    current_user.posse.solutions.create(solution_params)
+    unless current_user.posse.current_solution?
+      current_user.posse.solutions.create(solution_params)
+      current_user.posse.add_points(params[:solution][:points_earned])
+    end
  #   posse.add_score(solution.points_earned)
     head :ok
   end
--- app/models/posse.rb
+++ app/models/posse.rb
@@ -3,8 +3,15 @@ class Posse < ActiveRecord::Base
 
   has_many :solutions
 
+
   def add_score(points)
     self.scores += points
     self.save
+  end
+
+  def current_solution?
+    solutions.any? do |solution|
+      solution.created_at.to_date == Date.current
+    end
   end
 end
