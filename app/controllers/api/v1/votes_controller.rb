319882761ca9064a84c1a1488da89bef3ffb8c2c
Allison Larson <allisonlarson91@gmail.com>
2014-12-03 15:21:14 -0700

    Only add points for votes with new records

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -4,8 +4,10 @@ class Api::V1::VotesController < ApplicationController
   def create
     solution = Solution.find(params[:vote][:solution_id])
     vote = solution.votes.find_or_create_by(user_id: current_user.id)
-    solution.add_like_points
-    current_user.posse.add_points(50)
+    if vote.new_record?
+      solution.add_like_points 
+      current_user.posse.add_points(50) 
+    end
     render json: vote
   end
 end
