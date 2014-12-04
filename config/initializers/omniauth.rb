33b079927845caae13f4ef645082ce26ed3a7ca6
Hilary Denton <Hilarydenton90@gmail.com>
2014-12-04 08:46:05 -0700

    fix keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :github, "aedc133d73c7e782b0ed","6d53a6c382b7f2d0ecc0af8c606c7c71b88360b8"
+    provider :github, "b457f3e84fea9e9a4804","ce036908ed3eb23a910915ff72239a3b110195ff"
 end
