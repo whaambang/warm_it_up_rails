06353573dbf74c83788daf76d5dc6de2f70ead17
Will Faurot <wfaurot@gmail.com>
2015-01-12 16:37:43 -0800

    Add specs for adding/removing posse points

--- spec/models/posse_spec.rb
+++ spec/models/posse_spec.rb
@@ -12,4 +12,25 @@ describe "Posse", :type => :model do
 
     expect(posse).not_to be_valid
   end
+
+  describe "#add_points" do
+    it "increases a posse's score by given amount" do
+      posse = Posse.create(name: "McCarthy")
+
+      posse.add_points(50)
+
+      expect(posse.scores).to eq(50)
+    end
+  end
+
+  describe "#remove_points" do
+    it "decreases a posse's score by given amount" do
+      posse = Posse.create(name: "McCarthy")
+
+      posse.add_points(100)
+      posse.remove_points(50)
+
+      expect(posse.scores).to eq(50)
+    end
+  end
 end
